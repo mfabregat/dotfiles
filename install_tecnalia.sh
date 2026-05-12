820e2c89cac94f277403f4bf58080159309af190
Marc Fabregat <marc.fabregat@tecnalia.com>
2026-05-12 08:20:34 +0200

    updated tecnalia

--- install_tecnalia.sh
+++ install_tecnalia.sh
@@ -1,3 +1,15 @@
+# Remove unattended-upgrades
+sudo apt remove unattended-upgrades
+
+# Install git
+sudo apt install git git-lfs
+git config --global user.name "Marc Fabregat"
+git config --global user.email "marc.fabregat@tecnalia.com"
+
+# Follow instructions under https://github.com/IsmaelMartinez/teams-for-linux
+sudo wget -qO /etc/apt/keyrings/teams-for-linux.asc  https://repo.teamsforlinux.de/teams-for-linux.asc
+echo "deb [signed-by=/etc/apt/keyrings/teams-for-linux.asc arch=$(dpkg --print-architecture)] https://repo.teamsforlinux.de/debian/ stable main" | sudo tee /etc/apt/sources.list.d/teams-for-linux-packages.list
+
 ssh-keygen -t ed25519 -C "marc.fabregat@tecnalia.com"
 
 # Add ed25519.pub to https://git.code.tecnalia.dev/-/user_settings/ssh_keys
